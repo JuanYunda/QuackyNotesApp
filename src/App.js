0d73caf1a5c70fa1158d8d8dc17fbfc1e17fab69
Sheilly Ortega 2040051-3743 <81594679+DaylinSheilly@users.noreply.github.com>
2023-04-22 10:50:46 -0500

    modificado por shei

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <ul>
-    <li>Elemento 1</li>
-    <li>Elemento 2</li>
-    <li>Elemento 3</li>
-  </ul>
+      <ul>
+        <li>Elemento 1</li>
+        <li>Elemento 2</li>
+        <li>sheily modificacion</li>
+      </ul>
     </div>
   );
 }
