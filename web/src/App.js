c2ae465a51d8bbce7ca01f936feec2baa2a1965e
Andres <andres.camargo@correounivalle.edu.co>
2023-04-28 16:56:04 -0500

    login fixes register

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import "./App.css";
 import Login from "./components/Login";
 import Notes from "./components/Notes";
-
+import Register from './components/Register';
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/notes" element={<Notes />} />
+          <Route path='/register' element={<Register/>}/>
         </Routes>
       </Router>
     </div>
