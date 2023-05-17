c31b6967022f1c53b65d475a28290694f45562e7
Juan Mazuera 2043008 <81594863+JuanYunda@users.noreply.github.com>
2023-05-17 09:15:42 -0500

    cambios en las restricciones de los datos de registro en el backend.

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -1,16 +1,31 @@
-
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 from .models import Usuarios
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = Usuarios
-        fields = ['id', 'nombre','apellidos','celular','username','fecha_registro', 'password']
+        fields = ['id', 'nombre', 'apellidos', 'celular', 'username', 'fecha_registro', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
-            'fecha_registro': {'read_only':True}
+            'fecha_registro': {'read_only': True}
         }
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise ValidationError("La contraseña debe tener al menos 8 caracteres.")
+        if not any(char.isupper() for char in value):
+            raise ValidationError("La contraseña debe contener al menos una letra mayúscula.")
+        if not any(char.islower() for char in value):
+            raise ValidationError("La contraseña debe contener al menos una letra minúscula.")
+        if not any(char.isdigit() for char in value):
+            raise ValidationError("La contraseña debe contener al menos un número.")
+        if not any(char.isalnum() for char in value):
+            raise ValidationError("La contraseña debe contener al menos un carácter especial.")
+        return value
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
